fae1d62dab6674d543510267c4bbd5e7ea1564ef
JhonMonstro <jhonatanjhonjhon5050@gmail.com>
2023-10-02 09:46:08 -0300

    commit

--- arquivo.js
+++ arquivo.js
@@ -1,4 +1,17 @@
-function mostrarmensagem (){
-var texto= document.getElementById("dig").value
-document.getElementById("mensagem").innerHTML= `Essa foi a mensagem que você digitou: ${texto}`   
-}
+const express = require(`express`)
+const app = express()
+
+app.set('view engine','ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/login', function(req, res) {
+    res.render('index')
+})
+app.post('/paginaHome', function(req, res) {
+    console.log(req.body);
+   res.render('home', {data: req.body})
+})
+app.listen(3000, ()=>{
+    console.log('Meu servidor está rodando!');
+})
