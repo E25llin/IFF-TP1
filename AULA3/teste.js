a77ccb944f64519707a41fef9a18cf0b8465d468
E25llin <evellin.santos@gsuite.iff.edu.br>
2023-10-04 12:19:29 -0300

    a

--- AULA3/teste.js
+++ AULA3/teste.js
@@ -18,3 +18,54 @@ app.get('/soma', (req, res) => {
 
     res.send({ conta: result })
 })
+app.get('/', (request, response) => {
+    response.send('Envie os parâmetros n1 e n2 para a rota /subtrair')
+})
+
+app.get('/subtrair', (req, res) => {
+    console.log("n1=" + req.query.n1)
+    console.log("n2=" + req.query.n2)
+    result = parseFloat(req.query.n1) - parseFloat(req.query.n2)
+    if (isNaN(result)) {
+        result = "Valores inválidos."
+    } else {
+        result = `${req.query.n1} - ${req.query.n2} = ${result}`
+    }
+
+    res.send({ conta: result })
+})
+app.get('/', (request, response) => {
+    response.send('Envie os parâmetros n1 e n2 para a rota /vezes')
+})
+
+app.get('/vezes', (req, res) => {
+    console.log("n1=" + req.query.n1)
+    console.log("n2=" + req.query.n2)
+    result = parseFloat(req.query.n1) * parseFloat(req.query.n2)
+    if (isNaN(result)) {
+        result = "Valores inválidos."
+    } else {
+        result = `${req.query.n1} * ${req.query.n2} = ${result}`
+    }
+
+    res.send({ conta: result })
+})
+app.get('/', (request, response) => {
+    response.send('Envie os parâmetros n1 e n2 para a rota /subtrair')
+})
+
+app.get('/divisao', (req, res) => {
+    console.log("n1=" + req.query.n1)
+    console.log("n2=" + req.query.n2)
+    result = parseFloat(req.query.n1) / parseFloat(req.query.n2)
+    if (req.query.n2 == 0) {
+        result = "Esse número não é divisível."
+    } else if (isNaN(result)) {
+        result = "Valores inválidos."
+    } else {
+        result = `${req.query.n1} / ${req.query.n2} = ${result}`
+    }
+
+    res.send({ conta: result })
+})
+app.listen(8080)
