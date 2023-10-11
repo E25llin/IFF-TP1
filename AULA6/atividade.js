6ce933b14d70abdf295fd238c58f3f24862c2165
E25llin <evellin.santos@gsuite.iff.edu.br>
2023-10-11 16:01:35 -0300

    aa

--- AULA6/atividade.js
+++ AULA6/atividade.js
@@ -1,23 +1,25 @@
 const express = require('express')
 const app = express()
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.set("view engine","ejs")
 
 app.get('/', (request, response) => {
-    response.render('/soma')
+    response.render('form')
 })
 
 app.get('/soma', (req, res) => {
-    console.log("n1=" + req.query.n1)
-    console.log("n2=" + req.query.n2)
-    result = parseFloat(req.query.n1) + parseFloat(req.query.n2)
-    if (isNaN(result)) {
-        result = "Valores inválidos."
+    const data = {
+        n1: req.query.n1,
+        n2: req.query.n2,
+        result: parseFloat(req.query.n1) + parseFloat(req.query.n2)
+    }
+    if (isNaN(data.result)) {
+        data.result = "Valores inválidos."
     } else {
-        result = `${req.query.n1} + ${req.query.n2} = ${result}`
+        data.result = `${req.query.n1} + ${req.query.n2} = ${result}`
     }
 
-    res.send("form",{ conta: result })
+    res.send("lua",{ data })
 })
-res.send({ conta: result })
 app.listen(8080)
