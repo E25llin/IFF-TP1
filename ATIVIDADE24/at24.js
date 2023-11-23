f82c650703e17eeca7e8b27424b9a91e34ea8377
E25llin <143622878+E25llin@users.noreply.github.com>
2023-11-23 16:56:28 -0300

    Atividade
    Atividade

--- ATIVIDADE24/at24.js
+++ ATIVIDADE24/at24.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express ()
+const fs = require('fs')
 app.set("view engine", "ejs")
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (riquisicao, resposta ) => {
     resposta.render('index')
@@ -21,6 +23,27 @@ app.get("/sob", (riquisicao, resposta ) => {
 
 app.get("/mac", (riquisicao, resposta ) => {
     resposta.render('mac')
+})
+
+app.get("/pont", (riquisicao, resposta ) => {
+    resposta.render('pont')
+})
+
+app.get('/cad', (request, response) => {
+    resultado = ""
+    response.render('cad', { resultado })
+})
+
+app.post('/salvar', (req, res) => {
+    let nomeNoForm = req.body.nome
+    let telefoneNoForm = req.body.telefone
+    let localNoForm = req.body.local
+    let diaNoForm = req.body.dia
+    
+    let cadastro = {nome: nomeNoForm, telefone: telefoneNoForm, local: localNoForm, dia: diaNoForm}
+    fs.appendFileSync('visitas.json', `\n${JSON.stringify(cadastro)}`)
+    resultado = `Entraremos em contato para confirmar sua visita, ${nomeNoForm}.`
+    res.render('cad', { resultado })
 })
 
 app.listen(8080)
